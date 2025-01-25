a6b1853346aa8f8eee0dbe5ca0dba9327a8eca30
ErwanT <temple.erwan@gmail.com>
2025-01-25 17:47:04 +0100

    IngrendientBench

--- Assets/Scripts/BubbleTea/IngredientContainer.cs
+++ Assets/Scripts/BubbleTea/IngredientContainer.cs
@@ -1,8 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IngredientContainer : MonoBehaviour
 {
-    [SerializeField] private IngredientData _data;
+    [SerializeField] private Image _icon;
+    [SerializeField] private TextMeshProUGUI _name;
+    private IngredientData _data;
+
+    public void Populate(IngredientData data)
+    {
+        _data = data;
+        _icon.sprite = data.Sprite;
+        _name.text = data.Name;
+    }
+
     public void OnClick()
     {
         BubbleTeaManager.Instance.AddIngredient(_data);
--- Assets/Scripts/BubbleTea/IngredientData.cs
+++ Assets/Scripts/BubbleTea/IngredientData.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "IngredientData",menuName ="Data/Ingredient")]
 public class IngredientData : ScriptableObject
 {
+    [SerializeField] private Sprite _sprite;
+    [SerializeField] private string _name;
     [SerializeField] private IngredientType _type;
     [SerializeField, Range(-5, 5)] private float _acidity;
     [SerializeField, Range(-5, 5)] private float _sugar;
     [SerializeField, Range(-5, 5)] private float _fruit;
     
+    public Sprite Sprite => _sprite;
+    public string Name => _name;
+
     public IngredientType Type => _type;
     public float Acidity => _acidity;
     public float Sugar => _sugar;
