eb60fdc8899c2b1d3014347d8f4f299d319637f8
Garywiss <garywiss6@gmail.com>
2025-01-25 19:33:38 +0100

    First Request & Client Data

---
+++ Assets/Scripts/Client/ClientData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ClientData", menuName = "Scriptable Objects/ClientData")]
+public class ClientData : ScriptableObject
+{
+  [SerializeField] private RequestData _requestData;
+  public RequestData RequestData => _requestData;
+  [SerializeField] private DialogData _welcomeDialog;
+  public DialogData WelcomeDialog => _welcomeDialog;
+  [SerializeField] private DialogData _successDialog;
+  public DialogData SuccessDialog => _successDialog;
+  [SerializeField] private DialogData _failedDialog;
+  public DialogData FailedDialog => _failedDialog;
+}
