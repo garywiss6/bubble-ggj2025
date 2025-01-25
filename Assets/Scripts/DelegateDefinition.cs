54e44ddc35ab5ec6e7a70a1f7867171bb46b1d24
Garywiss <garywiss6@gmail.com>
2025-01-25 14:57:39 +0100

    WIP Systeme de dialogue

--- Assets/Scripts/DelegateDefinition.cs
+++ Assets/Scripts/DelegateDefinition.cs
@@ -1,4 +1,6 @@
 public static class DelegateDefinition
 {
     public delegate void void_D_Ingredient(IngredientData ingredient);
+
+    public delegate void void_D_void();
 }
---
+++ Assets/Scripts/Dialog/DialogBubble.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class DialogBubble : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _DialogText;
+    private Tween _currentTween;
+    private DelegateDefinition.void_D_void _onDialogFinished;
+
+    public DelegateDefinition.void_D_void OnDialogFinished
+    {
+        get { return _onDialogFinished; }
+        set { 
+        _onDialogFinished = value;
+        }
+    }
+
+    public bool IsTweening
+    {
+        get
+        {
+            return _currentTween != null && _currentTween.IsPlaying(); 
+        }
+    }
+
+    public void ShowText(string text)
+    {
+        if (_currentTween != null)
+            _currentTween.Kill();
+        _DialogText.text = "";
+        _currentTween = _DialogText.DOText(text, 1.0f).OnComplete(() => { _onDialogFinished?.Invoke(); });
+    }
+}
